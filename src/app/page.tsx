9ad01264e6454e4d761ce4d3f3542dacf0519469
kingcinematics4-arch <kingcinematics4@gmail.com>
2026-05-18 18:43:50 +0530

    Updated something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-// import MaintenancePage from "./maintenance/page";
+import MaintenancePage from "./maintenance/page";
 
-// export default function Home() {
-//   return <MaintenancePage />;
-// }import React from 'react';
-import AuthScreen from './components/AuthScreen';
-
-export default function Page() {
-  return <AuthScreen />;
+export default function Home() {
+  return <MaintenancePage />;
 }
+// }import React from 'react';
+// import AuthScreen from './components/AuthScreen';
+
+// export default function Page() {
+//   return <AuthScreen />;
+// }
