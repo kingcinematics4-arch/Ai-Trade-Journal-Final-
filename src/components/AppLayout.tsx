61882ee9155b48cfe2bbdba5a6820afe5529b469
kingcinematics4-arch <kingcinematics4@gmail.com>
2026-06-10 23:33:14 +0530

    Updated something

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import Sidebar from './Sidebar';
 
 interface AppLayoutProps {
   children: React.ReactNode;
-  activePath?: string;
 }
 
-export default function AppLayout({ children, activePath }: AppLayoutProps) {
+export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="flex h-screen bg-background overflow-hidden w-full relative">
-      <Sidebar activePath={activePath} />
       <main className="flex-1 min-w-0 overflow-y-auto overflow-x-hidden scrollbar-thin pt-16 lg:pt-0">
         {children}
       </main>
